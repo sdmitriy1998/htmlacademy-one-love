1a85ca70e4a0601b24e2a6d93f85bcf51a2082fb
sdmitriy1998 <58834662+sdmitriy1998@users.noreply.github.com>
2020-04-28 06:28:41 +0300

    Анимация для элементов

--- script.js
+++ script.js
@@ -13,16 +13,19 @@
 	  if(!open){
 		wrapper.classList.add("circle-menu__list_opened");
 		circle_menu.classList.add("circle-menu_margin");
+		button.classList.add("circle-menu__button_opened");
 	  }
 	  else{
 		wrapper.classList.remove("circle-menu__list_opened");
 		circle_menu.classList.remove("circle-menu_margin");
+		button.classList.remove("circle-menu__button_opened");
 	  }
 	  open = !open;
 	}
 	function closeWrapper(){
 		wrapper.classList.remove("circle-menu__list_opened");
 		circle_menu.classList.remove("circle-menu_margin");
+		button.classList.remove("circle-menu__button_opened");
 	}
 
 })();
